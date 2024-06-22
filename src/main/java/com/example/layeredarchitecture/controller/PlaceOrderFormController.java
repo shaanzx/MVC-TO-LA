fdc292a27e3ee01882a2c5d4f866fdb8e3569849
Shanzx <anuhaslakshan07@gmail.com>
2024-06-22 12:41:02 +0530

    Update

--- src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -33,11 +33,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
-
 public class PlaceOrderFormController {
 
     public AnchorPane root;
